0634d6a0ea71fbeaa59c984c751ffa34c93aa21a
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-07-05 16:05:23 +0100

    implement botton nav item and navigation

--- app/src/main/java/com/example/greyandroidengineertechnicalassessment/MainActivity.kt
+++ app/src/main/java/com/example/greyandroidengineertechnicalassessment/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.greyandroidengineertechnicalassessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.greyandroidengineertechnicalassessment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +17,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        //set up bottom nav with nav controller
+        val bottomNavigationView = binding.bottomNavigationView
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 }
