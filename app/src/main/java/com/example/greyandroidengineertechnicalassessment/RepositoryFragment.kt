13448c8e5afac8f0191e0a59c48ea402131de87f
seunAwonugba <86803598+seunAwonugba@users.noreply.github.com>
2022-07-06 02:31:28 +0100

    implement repo screen and users screen

--- app/src/main/java/com/example/greyandroidengineertechnicalassessment/RepositoryFragment.kt
+++ app/src/main/java/com/example/greyandroidengineertechnicalassessment/RepositoryFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.greyandroidengineertechnicalassessment.databinding.FragmentRepositoryBinding
 import com.example.greyandroidengineertechnicalassessment.databinding.FragmentUsersBinding
+import com.google.android.material.snackbar.Snackbar
 
 class RepositoryFragment : Fragment(R.layout.fragment_repository) {
 
@@ -16,6 +17,10 @@ class RepositoryFragment : Fragment(R.layout.fragment_repository) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentRepositoryBinding.bind(view)
+
+        binding.button3.setOnClickListener {
+            Snackbar.make(binding.root, "Btton clicked", Snackbar.LENGTH_LONG).show()
+        }
 
     }
 
